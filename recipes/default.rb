9d37284e669bfd0ea97800d86c7de3c34e9e26ea
Corey Hemminger <hemminger@hotmail.com>
2024-07-11 14:52:07 -0500

    test
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,22 +1,2 @@
-ruby_block 'delete_sandbox_files' do
-  block do
-    automate_dir = '/hab/pkgs/chef/automate-ui/2.0.0'
-    deleted_files = []
-    if ::File.exist?(automate_dir)
-      Find.find(automate_dir) do |path|
-        if ::File.basename(path) == 'sandbox.html'
-          file path do
-            action :delete
-          end
-          deleted_files.push(path)
-        end
-      end
-
-      file '/var/log/chef-quarantine-polyfill' do
-        content deleted_files.join("\n")
-        action :create
-        not_if { deleted_files.empty? }
-      end
-    end
-  end
-end
+# Run Polyfill Remediation resource
+polyfill_remediation 'default'
