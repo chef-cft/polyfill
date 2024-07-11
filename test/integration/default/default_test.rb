b040cf75d0ee2f4435262f11e7d6538248d8d0e0
Corey Hemminger <hemminger@hotmail.com>
2024-07-11 15:08:17 -0500

    test
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -4,10 +4,10 @@ describe service('chef-automate') do
   it { should be_enabled }
 end
 
-describe file("#{Dir.glob('/hab/pkgs/chef/automate-ui/2.0.0/*')}/dist/assets/chef-ui-library/chef/sandbox.html") do
+describe file("#{Dir.glob('/hab/pkgs/chef/automate-ui/2.0.0/*').first}/dist/assets/chef-ui-library/chef/sandbox.html") do
   it { should_not exist }
 end
 
-describe file("#{Dir.glob('/hab/pkgs/chef/automate-ui/2.0.0/*')}/dist/assets/chef-ui-library/collection/sandbox.html") do
+describe file("#{Dir.glob('/hab/pkgs/chef/automate-ui/2.0.0/*').first}/dist/assets/chef-ui-library/collection/sandbox.html") do
   it { should_not exist }
 end
